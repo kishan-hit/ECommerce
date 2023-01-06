40b0337b4bf5ffeeb5a46b1d4fc30b80cb70512c
Kishan Kumar <kishan.kumar@zimtive.com>
2023-01-06 17:33:14 +0530

    updated

--- src/components/ProductCarouselComponent.js
+++ src/components/ProductCarouselComponent.js
@@ -1,40 +1,54 @@
 import Carousel from 'react-bootstrap/Carousel';
+import { LinkContainer } from 'react-router-bootstrap';
 
 const ProductCarouselComponent = () => {
+    const cursorP = {
+        cursor: "pointer"
+    }
     return (
         <Carousel>
             <Carousel.Item>
                 <img
+                    crossOrigin='anonymous'
                     className="d-block w-100"
+                    style={{height: "300px", objectFit: "cover"}}
                     src="/images/carousel/carousel-1.png"
                     alt="First slide"
                 />
                 <Carousel.Caption>
-                    <h3>First slide label</h3>
+                    <LinkContainer style={cursorP} to='/product-details'>
+                        <h3>Bestseller in laptops category</h3>
+                    </LinkContainer>
                     <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
+                    style={{height: "300px", objectFit: "cover"}}
                     src="/images/carousel/carousel-2.png"
                     alt="Second slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Second slide label</h3>
+                    <LinkContainer style={cursorP} to='/product-details'>
+                        <h3>Bestseller in books category</h3>
+                    </LinkContainer>
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </Carousel.Caption>
             </Carousel.Item>
             <Carousel.Item>
                 <img
                     className="d-block w-100"
+                    style={{height: "300px", objectFit: "cover"}}
                     src="/images/carousel/carousel-3.png"
                     alt="Third slide"
                 />
 
                 <Carousel.Caption>
-                    <h3>Third slide label</h3>
+                    <LinkContainer style={cursorP} to='/product-details'>
+                        <h3>Bestseller in cameras category</h3>
+                    </LinkContainer>
                     <p>
                         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
                     </p>
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,32 @@
 import ProductCarouselComponent from "../components/ProductCarouselComponent";
 import CategoryCardComponent from "../components/CategoryCardComponent";
+// import Card from 'react-bootstrap/Card';
+// import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from "react-bootstrap/esm/Container";
 
 const HomePage = () => {
-    return(
+    const categories = [
+        "Tablets",
+        "Monitors",
+        "Games",
+        "Printers",
+        "Software",
+        "Cameras",
+        "Books",
+        "Videos",
+    ];
+    return (
         <>
             <ProductCarouselComponent />
-            <CategoryCardComponent />
+            <Container>
+                <Row xs={1} md={2} className="g-4 mt-5">
+                    {
+                        categories.map((category, idx) => (
+                            <CategoryCardComponent key={idx} category={category} idx={idx} />
+                    ))}
+                </Row>
+            </Container>
         </>
     )
 };
