2658834fcb5d2d904125ce1eb14ecde34c3141ae
Kishan Kumar <kishan.kumar@zimtive.com>
2023-01-11 23:12:48 +0530

    updated

--- src/components/user/UserChatComponent.js
+++ src/components/user/UserChatComponent.js
@@ -17,7 +17,7 @@ const UserChatComponent = () => {
                     <div className='chat-msg'>
                         {
                             Array.from({ length: 20 }).map((_, id) => (  
-                                <div>
+                                <div key={id}>
                                     <p>
                                         <b>You wrote:</b> Hello, world! This is a toast message.
                                     </p>
