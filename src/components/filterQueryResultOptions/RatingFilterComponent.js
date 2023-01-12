8b90333abb162fc32dc472f2babda128b8746d8f
Kishan Kumar <kishan.kumar@zimtive.com>
2023-01-12 12:45:58 +0530

    updated

--- src/components/filterQueryResultOptions/RatingFilterComponent.js
+++ src/components/filterQueryResultOptions/RatingFilterComponent.js
@@ -4,19 +4,19 @@ import { Fragment } from "react";
 
 const RatingFilterComponent = () => {
   return (
-   <>
+    <>
       <span className="fw-bold">Rating</span>
-      {Array.from({length:5}).map((_,idx)=>(
+      {Array.from({ length: 5 }).map((_, idx) => (
         <Fragment key={idx}>
-        <Form.Check type="checkbox" id={`check-api-${idx}`}>
-        <Form.Check.Input type="checkbox" isValid />
-        <Form.Check.Label style={{cursor: "pointer"}}>
-        <Rating readonly size={20} initialValue={5-idx}/>
-        </Form.Check.Label>
-      </Form.Check>
+          <Form.Check type="checkbox" id={`check-api-${idx}`}>
+            <Form.Check.Input type="checkbox" isValid />
+            <Form.Check.Label style={{ cursor: "pointer" }}>
+              <Rating readonly size={20} initialValue={5 - idx} />
+            </Form.Check.Label>
+          </Form.Check>
         </Fragment>
       ))}
-   </>
+    </>
   );
 };
 
